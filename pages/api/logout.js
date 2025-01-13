36b795df72eb32ec2cf70e65f15bce5ae7bfd12e
GravityG <61642138+GravityGYT@users.noreply.github.com>
2025-01-13 22:47:59 +0000

    logout.js

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,15 +1,4 @@
-import { serialize } from "cookie";
-
 export default function handler(req, res) {
-    res.setHeader(
-        "Set-Cookie",
-        serialize("session", "", {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            maxAge: -1, // Expire immediately
-            path: "/",
-        })
-    );
-
-    res.json({ message: "Logged out successfully" });
+    // Optional if you use session-based auth
+    res.status(200).send({ message: 'Logged out successfully' });
 }
