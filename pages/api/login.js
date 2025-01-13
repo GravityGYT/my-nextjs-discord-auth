3fc9b79edddaac40c5a6a876bd637a23811b3097
GravityG <61642138+GravityGYT@users.noreply.github.com>
2025-01-13 22:54:10 +0000

    login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,7 +1,10 @@
+const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
+const REDIRECT_URI = `${process.env.NEXT_PUBLIC_BASE_URL}/api/callback`;
+
 export default function handler(req, res) {
-    const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
-    const REDIRECT_URI = `${process.env.NEXT_PUBLIC_BASE_URL}/api/callback`;
-    const SCOPE = 'identify';
-    const DISCORD_LOGIN_URL = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=${SCOPE}`;
-    res.redirect(DISCORD_LOGIN_URL);
+    const discordAuthUrl = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
+        REDIRECT_URI
+    )}&response_type=code&scope=identify`;
+
+    res.redirect(discordAuthUrl);
 }
