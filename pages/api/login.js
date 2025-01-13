c91b0e2db7a9e86c4f9897da128b0731f201a786
GravityG <61642138+GravityGYT@users.noreply.github.com>
2025-01-13 21:29:42 +0000

    login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,5 @@
 const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
-const REDIRECT_URI = `${process.env.NEXT_PUBLIC_BASE_URL}/api/callback`;
+const REDIRECT_URI = process.env.NEXT_PUBLIC_BASE_URL;
 
 export default function handler(req, res) {
     const discordAuthUrl = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
